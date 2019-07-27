976f82b43c527a4dd13e2c838fae3a8c128e7e76
Luciano Moraes da Luz Brum <lucianobrum18@gmail.com>
2019-07-26 22:11:09 -0300

    update

--- R/data_integration_visualization.r
+++ R/data_integration_visualization.r
@@ -42,13 +42,16 @@ if(!require(gganimate)){
   install.packages("gganimate", repos = "http://cran.us.r-project.org")
   require(gganimate)
 }
-
-brazilian_states <- read.csv("csv/brazilian_states.csv",sep=",", stringsAsFactors=FALSE, encoding = "UTF-8")
-brazilian_cities <- read.csv("csv/brazilian_cities.csv",sep=",", stringsAsFactors=FALSE, encoding = "Latin-1")
-universities <- read.xlsx("csv/universities_after.xlsx", sheetIndex = 1, encoding = "UTF-8")
-university_research <- read.xlsx("csv/university_research.xlsx", sheetIndex = 1, encoding = "UTF-8")
-research_names <- read.xlsx("csv/research_names.xlsx", sheetIndex = 1, encoding = "UTF-8")
-shape_brazil <- readOGR("shapefile/Brazil.shp", "Brazil",use_iconv=TRUE)
+if(!require(ggrepel)){
+  install.packages("ggrepel", repos = "http://cran.us.r-project.org")
+  require(ggrepel)
+}
+brazilian_states <- read.csv("../csv/brazilian_states.csv",sep=",", stringsAsFactors=FALSE, encoding = "UTF-8")
+brazilian_cities <- read.csv("../csv/brazilian_cities.csv",sep=",", stringsAsFactors=FALSE, encoding = "Latin-1")
+universities <- read.xlsx("../csv/universities_after.xlsx", sheetIndex = 1, encoding = "UTF-8")
+university_research <- read.xlsx("../csv/university_research.xlsx", sheetIndex = 1, encoding = "UTF-8")
+research_names <- read.xlsx("../csv/research_names.xlsx", sheetIndex = 1, encoding = "UTF-8")
+shape_brazil <- readOGR("../shapefile/Brazil.shp", "Brazil",use_iconv=TRUE)
 colnames(brazilian_states)[1]<-"codigo_uf"
 
 all_data <- universities %>% left_join(brazilian_cities, by = c("Codigo_Cidade" = "codigo_ibge"))
@@ -80,7 +83,7 @@ ggplot() +
   scale_size_continuous(name = "Conceito", breaks = mybreaks) +
   coord_map() + 
   guides( colour = guide_legend(), alpha = FALSE) +
-  ggtitle("Programas de Pós-Graduação em Computação") +
+  ggtitle("Programas de P�s-Graduação em Computa��o") +
   labs(x = "Longitude", y = "Latitude") +
   theme(
     legend.position = c(0.9, 0.15),
@@ -96,11 +99,11 @@ p <- ggplot( data = all_data, aes(group = Ano)) +
   geom_path( data = shape_brazil, aes( x = long, y = lat, group = group), color = "black", size = 0.05) +
   geom_point( data = all_data, aes(x = long_university, y = lat_university, size = 4, color = as.factor(Nota), alpha = 0.05, group = Ano, shape = as.factor(id_mestrado_doutorado))) +
   scale_color_manual(name = "Conceito", breaks = mybreaks, values = c("red","orange","yellow","dark green","light green")) +
-  scale_shape_manual(name = "Nível", values = c(15, 17, 18, 19), labels = c("Mestrado Profissional","Mestrado","Doutorado","Mestrado + Doutorado")) +
+  scale_shape_manual(name = "N�vel", values = c(15, 17, 18, 19), labels = c("Mestrado Profissional","Mestrado","Doutorado","Mestrado + Doutorado")) +
   coord_map() +
   shadow_mark(past = TRUE) +
   guides( colour = guide_legend(override.aes = list(size = 8)), shape = guide_legend(override.aes = list(size=8)), alpha = FALSE, size = FALSE) +
-  ggtitle( "Programas de Pós-Graduação em Computação em {round(frame_time,0)}") +
+  ggtitle( "Programas de P�s-Gradua��o em Computa��o em {round(frame_time,0)}") +
   labs( x = "Longitude", y = "Latitude") +
   transition_time(Ano) +
   theme(
@@ -122,12 +125,12 @@ animate(p, nframes = 52, fps = 1, width = 1800, height = 900, renderer = ffmpeg_
 ggplot() + 
   geom_polygon( data = shape_brazil, aes(x = long, y = lat, group = group), fill = "grey", alpha = 0.3) +
   geom_path( data = shape_brazil, aes( x = long, y = lat, group = group), color = "black", size = 0.1) +
-  geom_point( data = all_data[all_data$nome=="Inteligencia Artificial",], aes(x = long_university, y = lat_university, size = as.factor(Nota), color = as.factor(Nota), alpha = 0.7)) +
+  geom_point( data = all_data[all_data$nome=="Intelig�ncia Artificial",], aes(x = long_university, y = lat_university, size = as.factor(Nota), color = as.factor(Nota), alpha = 0.7)) +
   scale_colour_manual(name = "Conceito", values = c("red","blue","dark green","green","yellow")) +
   scale_size_manual(name = "Conceito", values = c(3,4,5,6,7)) +
   coord_map() + 
   guides( alpha = FALSE) +
-  ggtitle("Programas de Pós-Graduação em Inteligência Artificial") +
+  ggtitle("Programas de P�s-Gradua��o em Intelig�ncia Artificial") +
   labs(x = "Longitude", y = "Latitude") +
   theme(
     legend.position = c(0.9, 0.15),
@@ -140,12 +143,12 @@ ggplot() +
 ggplot() + 
   geom_polygon( data = shape_brazil, aes(x = long, y = lat, group = group), fill = "grey", alpha = 0.3) +
   geom_path( data = shape_brazil, aes( x = long, y = lat, group = group), color = "black", size = 0.1) +
-  geom_point( data = all_data[all_data$nome=="Teoria da Computação",], aes(x = long_university, y = lat_university, size = Nota, color = Nota, alpha = 0.3)) +
+  geom_point( data = all_data[all_data$nome=="Teoria da Computa��o",], aes(x = long_university, y = lat_university, size = Nota, color = Nota, alpha = 0.3)) +
   scale_color_viridis(name = "Conceito", breaks = mybreaks) +
   scale_size_continuous(name = "Conceito", breaks = mybreaks) +
   coord_map() + 
   guides( colour = guide_legend(), alpha = FALSE) +
-  ggtitle("Programas de Pós-Graduação em Teoria da Computação") +
+  ggtitle("Programas de P�s-Gradua��o em Teoria da Computação") +
   labs(x = "Longitude", y = "Latitude") +
   theme(
     legend.position = c(0.9, 0.15),
@@ -165,7 +168,7 @@ ggplot() +
   scale_size_continuous(name = "Conceito", breaks = mybreaks) +
   coord_map() + 
   guides( colour = guide_legend(), size = guide_legend(), alpha = FALSE, label = FALSE, text = FALSE) +
-  ggtitle("Programas de Pós-Graduação com Mestrado e Doutorado") +
+  ggtitle("Programas de Pós-Gradua��o com Mestrado e Doutorado") +
   labs(x = "Longitude", y = "Latitude") +
   theme(
     legend.position = c(0.9, 0.15),
